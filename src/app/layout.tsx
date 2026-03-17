062980da54c68f50599aeafca21151df0f316097
chakronwork <chakronwork@gmail.com>
2026-03-17 13:12:11 +0700

    1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
     siteName: 'Chakron Yuraket Portfolio',
     images: [
       {
-        url: '/images/og-image.png',
+        url: 'public/images/og-image.png',
         width: 1200,
         height: 630,
         alt: 'Portfolio Preview of Chakron Yuraket',
